8706dec3d05fe662e29654376e282a448030f4da
Begoingto <begoingto.me@gmail.com>
2023-11-08 02:29:25 +0700

    Fix node version and source URL parameters in NextJsDeploy.groovy

--- vars/nextJsDeploy.groovy
+++ vars/nextJsDeploy.groovy
@@ -1,7 +1,7 @@
 import org.begoingto.NextJsDeployer
 
 def call(Map params){
-    def nodeVersion = params.nodeJSVersion ?: 'nodejs-18.12.1'
+    def nodeVersion = params.nodeVersion ?: 'nodejs-18.12.1'
     def nextJsDeployer = new NextJsDeployer(steps, this, nodeVersion)
-    nextJsDeployer.deploy(params.sourceUrl, params.branch)
+    nextJsDeployer.deploy(params.source, params.branch)
 }
