bad9cf4b3c4373fa3a6b4f74f50611ec1e75e1fc
Begoingto <begoingto.me@gmail.com>
2023-11-08 12:46:57 +0700

    add create docker file

--- src/org/begoingto/NodeJsDeployer.groovy
+++ src/org/begoingto/NodeJsDeployer.groovy
@@ -27,5 +27,68 @@ class NodeJsDeployer {
             steps.sh "npm install"
             steps.sh "npm run build"
         }
+
+        // Create Dockerfile
+        createDockerfile(projectType)
+    }
+
+    private createDockerfile(String projectType){
+        def dockerfileContent = ""
+        if(projectType == 'react'){
+            dockerfileContent = """
+            FROM nginx:1.23.2
+            RUN rm -rf /usr/share/nginx/html/*
+            COPY ./build/ /usr/share/nginx/html 
+            EXPOSE 80 
+            CMD ["nginx", "-g", "daemon off;"]
+            """
+            steps.writeFile(file: 'Dockerfile', text: dockerfileContent)
+        }else if(projectType == 'next') {
+            dockerfileContent = """
+            # using lightweight alpine image for final image
+            FROM node:18-alpine
+            # set app working directory /app
+            WORKDIR /app
+            # copy the app from builder stage
+            COPY ./.next ./.next
+            COPY ./public ./public
+            COPY ./node_modules ./node_modules
+            COPY ./package.json ./package.json
+            # start the application
+            CMD ["npm", "start"]
+            EXPOSE 3000
+            """
+            steps.writeFile(file: 'Dockerfile', text: dockerfileContent)
+        }else if(projectType == 'angular') {
+            dockerfileContent = """
+            FROM nginx:alpine
+            RUN rm -rf /usr/share/nginx/html/*
+            # Copy the output from Stage 1 to replace the default nginx contents.
+            COPY ./dist/* /usr/share/nginx/html/
+            # Expose port 80
+            EXPOSE 80
+            # Start Nginx and keep it running in the foreground
+            CMD ["nginx", "-g", "daemon off;"]
+            """
+            steps.writeFile(file: 'Dockerfile', text: dockerfileContent)
+        }else if(projectType == 'nuxt'){
+            dockerfileContent = """
+            # using lightweight alpine image for final image
+            FROM node:18-alpine
+            # set app working directory /app
+            WORKDIR /app
+            # copy the app from builder stage
+            COPY ./.output ./.output
+            COPY ./public ./public
+            COPY ./node_modules ./node_modules
+            COPY ./package.json ./package.json
+            EXPOSE 3000
+            # start the application
+            CMD ["node", ".output/server/index.mjs"]
+            """	
+            steps.writeFile(file: 'Dockerfile', text: dockerfileContent)
+        }else{
+            echo 'not support project type'	
+        }
     }
 }
