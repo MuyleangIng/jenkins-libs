670d713c6aaa24b286b04660711e2f2d783f2770
Begoingto <begoingto.me@gmail.com>
2023-11-09 11:54:14 +0700

    withCredentials

--- vars/ansibleExecution.groovy
+++ vars/ansibleExecution.groovy
@@ -1,5 +1,17 @@
 import org.begoingto.Ansible
 
-def call() {
+def call(Map params) {
+    def username = 'default'
+    def password = 'default'
+    
+    withCredentials([usernamePassword(credentialsId: params.credentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        steps.echo "registry Name: ${USERNAME}"
+        username = USERNAME
+        password = PASSWORD
+    }
+
+    echo "Username: ${username}"
+    echo "Password: ${password}"
+
     return new Ansible(steps,this)
 }
