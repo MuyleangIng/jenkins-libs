669a1830ef0e9b9c4262c4b2b5b0f0135513bb16
Begoingto <begoingto.me@gmail.com>
2023-11-09 07:57:51 +0700

    ansibleExecution 2

--- vars/ansibleExecution.groovy
+++ vars/ansibleExecution.groovy
@@ -1,5 +1,8 @@
+import org.begoingto.Ansible
+
 def call() {
-    def ansible = new org.begoingto.Ansible(this,script)
+    def ansible = new Ansible(this,script)
+    
     ansible.testScript()
     def exist = ansible.exitst()
     echo "Ansible is available: ${exist}"
