fd89d4c42a00007f5707011cbfb716ee5858d04f
Begoingto <begoingto.me@gmail.com>
2023-11-10 03:17:40 +0700

    build try catch

--- vars/BackendBuild.groovy
+++ vars/BackendBuild.groovy
@@ -1,22 +1,36 @@
+import org.begoingto.Notification
+
 def call(Map params) {
-    sh "${params.gradleHome}/bin/gradle clean build"
-    writeDockerfile()
-    withCredentials([usernamePassword(credentialsId: params.registryCredentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        // docker build
-        dockerBuild(username: USERNAME, 
-            imageName: params.imageName, 
-            tag: params.tag,
-            registryName: params.registryName    
-        )
-        // docker push
-        if(params.registryName == 'hub.docker.io'){
-            sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-            sh "docker push ${USERNAME}/${params.imageName}:${params.tag}"
-        }else{
-            sh "docker login -u ${USERNAME} -p ${PASSWORD} ${params.registryName}"
-            sh "docker push ${params.registryName}/${params.imageName}:${params.tag}"
+    try {
+        sh "${params.gradleHome}/bin/gradle clean build"
+        writeDockerfile()
+        withCredentials([usernamePassword(credentialsId: params.registryCredentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+            // docker build
+            dockerBuild(username: USERNAME, 
+                imageName: params.imageName, 
+                tag: params.tag,
+                registryName: params.registryName    
+            )
+            // docker push
+            if(params.registryName == 'hub.docker.io'){
+                sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+                sh "docker push ${USERNAME}/${params.imageName}:${params.tag}"
+            }else{
+                sh "docker login -u ${USERNAME} -p ${PASSWORD} ${params.registryName}"
+                sh "docker push ${params.registryName}/${params.imageName}:${params.tag}"
+            }
         }
+    } catch (Exception e) {
+        def notify = new Notification(steps, this)
+        notify.sendTelegram(
+            botId: params.botId, 
+            botToken: params.botToken, 
+            chatId: params.chatId, 
+            message: "Build failed⛔, Error: ${e.getMessage()}"
+        )
+        throw e
     }
+    
 }
 
 def writeDockerfile(){
